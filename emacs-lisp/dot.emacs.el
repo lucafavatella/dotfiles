6357f76c00c06589ca18a6ed5d344f9e14ae57d9
Luca Favatella <lucafavatella@users.noreply.github.com>
2015-07-20 22:31:00 +0100

    Delete minor haskell bits from .emacs

--- emacs-lisp/dot.emacs.el
+++ emacs-lisp/dot.emacs.el
@@ -21,8 +21,6 @@
 ;(require 'erlang-flymake)
 ;(setq-default erlang-indent-level 2)
 
-;; haskell-mode
-;; Ref: https://github.com/haskell/haskell-mode
 (require 'package)
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
@@ -42,10 +40,3 @@
 	     (auto-fill-mode 1)
 	     (if (eq window-system 'x)
 		 (font-lock-mode 1))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(haskell-mode-hook (quote (turn-on-haskell-indentation))))
